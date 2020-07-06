9d0f736fb9790d6d4ed78e3d137d9df3aff970cd
username <username@europe>
2020-07-05 21:26:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Convert doc to text
+
+## Setup
+
+```bash
+sudo apt-get update
+sudo apt-get install libreoffice-writer
+composer require lyseontech/doctotext
+```
+
+## Use
+
+```php
+<?php
+
+use DocToText\DocToText;
+
+$docFile = 'document.doc';
+$outputDir = '/tmp';
+$doctotext = new DocToText($docFile, $outputDir));
+echo "Text:" . PHP_EOL;
+echo $doctotext->getText();
+echo "Total words:" . PHP_EOL;
+echo $doctotext->count();
+```
+
+## Explain
+
+This package is a wrapper to libreoffice.
---
+++ src/DocToText.php
@@ -0,0 +1,32 @@
+<?php
+namespace DocToText;
+
+class DocToText
+{
+    private $docFile;
+    private $outputDir;
+    private $text;
+
+    public function __construct($docFile, $outputDir = '/tmp')
+    {
+        $this->docFile = $docFile;
+        $this->outputDir = $outputDir;
+    }
+
+    public function getText()
+    {
+        $info = pathinfo($this->docFile);
+        exec('libreoffice --headless --convert-to "txt:Text (encoded):UTF8" --outdir ' . $this->outputDir . ' ' . $this->docFile);
+        $this->text = file_get_contents('/tmp/'.$info['filename'].'.txt');
+        unlink('/tmp/'.$info['filename'].'.txt');
+        return $this->text;
+    }
+
+    public function wordCount()
+    {
+        if ($this->text) {
+            return str_word_count($this->text);
+        }
+        return str_word_count($this->getText());
+    }
+}
