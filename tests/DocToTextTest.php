68449bb91c12768309378473f414f54da739baab
thyagocarlos <thiago-carlos@hotmail.com>
2020-07-09 15:12:24 -0300

    Use PHP_EOL constant

--- tests/DocToTextTest.php
+++ tests/DocToTextTest.php
@@ -3,7 +3,7 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use DocToText\DocToText;
 
-final class TestDocToText extends TestCase
+final class DocToTextTest extends TestCase
 {
     public function testConvertDocumentToText()
     {
@@ -15,7 +15,7 @@ final class TestDocToText extends TestCase
         // BOM is used at the beginning of the text flow to indicate the unicode   
         // characters the file uses. Libreoffice saves the text document using BOM.
         $BOM = "\xef\xbb\xbf";
-        $expected = $BOM . "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat lobortis urna, a dictum sem.\r\n";  
+        $expected = $BOM . "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat lobortis urna, a dictum sem." . PHP_EOL;  
         
         $this->assertEquals($expected, $docToText->getText());
     }
